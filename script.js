6da0a6c05b11e5c801158d22226e498bd1f0facd
adityajha <aj12102003@gmail.com>
2025-02-18 23:11:49 +0530

    All images added

--- script.js
+++ script.js
@@ -77,15 +77,66 @@ function loadingAnimation() {
     },"-=1.2");
 };
 function cursorAnimation() {
-    document.addEventListener("mousemove",function(dets){
-        gsap.to("#crsr",{
-            left:dets.x,
-            top:dets.y
+    Shery.mouseFollower({
+        skew:true,
+        ease:"cubic-bezier(0.23, 1, 0.320, 1)",
+        duration:1,
+    });
+    Shery.makeMagnet("#nav-part2 h4");
+
+    var videoContainer = document.querySelector("#video-container");
+    var video = document.querySelector("#video-container video")
+    videoContainer.addEventListener("mouseenter",function(){
+        videoContainer.addEventListener("mousemove",function(dets){
+            gsap.to(".mousefollower",{
+                display:"none"
+            });
+            gsap.to("#video-cursor",{
+                left:dets.x - 570,
+                y:dets.y - 300
+            })
         });
     });
-    
-    Shery.makeMagnet("#nav-part2 h4");
+    videoContainer.addEventListener("mouseleave",function(){
+        gsap.to(".mousefollower",{
+            display:"initial"
+        });
+        gsap.to("#video-cursor",{
+            left:"80%",
+            top:"-10%"
+        })
+    });
+
+    var flag = 0;
+    videoContainer.addEventListener("click",function(){
+        if(flag == 0) {
+            video.play();
+            video.style.opacity = 1;
+            document.querySelector("#video-cursor").innerHTML = `<i class="ri-pause-mini-fill"></i>`;
+            gsap.to("#video-cursor",{
+                scale:0.5
+            });
+            flag = 1;
+        }
+        else {
+            video.pause();
+            video.style.opacity = 0;
+            document.querySelector("#video-cursor").innerHTML = `<i class="ri-play-mini-fill"></i>`;
+            gsap.to("#video-cursor",{
+                scale:1
+            });
+            flag = 0;
+        }
+    });
+};
+function sheryAnimation() {
+    Shery.imageEffect(".image-div",{
+        style:5,
+        config:{"a":{"value":2,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7272756554789667},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.4,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
+        gooey:true
+    });
 };
 loadingAnimation();
 locomotiveAnimation();
-// cursorAnimation();
+cursorAnimation();
+sheryAnimation();
